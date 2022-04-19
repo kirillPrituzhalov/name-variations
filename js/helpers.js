4dda69edf990b233591a0d0038eca52fdc8042d8
prituzhalovkirill <prituzhalovkirill@gmail.com>
2022-04-19 13:43:02 +0600

    Project. Варианты имени

---
+++ js/helpers.js
@@ -0,0 +1,17 @@
+/* helpers.js */
+
+/** @param {string} name */
+export function getNumberOfChars(name) {
+    return name.length
+  };
+  
+  
+  /** @param {string} name */
+  export function getLower(name) {
+    return name.toLowerCase()
+  };
+  
+  /** @param {string} name */
+  export function getUpper(name) {
+    return name.toUpperCase()
+  };
